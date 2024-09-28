cdade36c90bae10aa51bd658c32cfd6edc799fcf
JustinGamboa <87157815+justinUCF@users.noreply.github.com>
2024-09-28 12:40:42 -0400

    Revert "Merge branch 'main' of https://github.com/Shellhacks2024-CapitalHealth/main"
    This reverts commit 38d2e8110610bc093cac93d5ba2b90966bf6ae46, reversing
    changes made to 96309147fb1f44783b22732f125e1616438e1104.

---
+++ income.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Income:
+
+    dataBase = pd.DataFrame
+
+    #CONSTRUCTOR
+    def __init__(self, file):
+        self.dataBase = pd.read_csv(file)
+
+    #Prints the first 5 rows of the data base
+    def printRow5(self):
+        self.dataBase.head()
+
+    #print (row, col)
+    def findSize(self):
+        print(self.dataBase.shape)
---
+++ main.py
@@ -0,0 +1,5 @@
+import spending as sp
+
+filePath_spending = ""
+
+spendingObj = sp.Spending(filePath_spending)
