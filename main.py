06834d4263191b43346429c6acd4db122584e7f2
JylerT <146783468+JylerTackson@users.noreply.github.com>
2024-09-29 05:23:10 -0400

    Updated API URL and Key

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ def main():
     analyzeria.compare_income_to_inflation()
 
     # API Details
-    API_URL = 'https://api.exchangeratesapi.io/v1/'
+    API_URL = 'http://data.fixer.io/api/latest?access_key={3ea93938bd2a266999351bddd2e43524}&base=EUR'
     HISTORICAL_API_URL = 'https://api.exchangeratesapi.io/v1/2013-12-24'
     BASE_CURRENCY = 'USD'
     TARGET_CURRENCY = 'EUR'
-    API_KEY = 'd80ef597d002cdaab062362079eaccc1'
+    API_KEY = '3ea93938bd2a266999351bddd2e43524'
 
     # Initialize Predictor
     predictor = ep(
-        api_url=HISTORICAL_API_URL,
+        api_url=API_URL,
         base_currency=BASE_CURRENCY,
         target_currency=TARGET_CURRENCY,
         api_key=API_KEY
